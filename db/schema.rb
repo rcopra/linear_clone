528802fcd2fa9f05d4f334295d805476d8fe578e
Rick Copra <137560494+rcopra@users.noreply.github.com>
2025-06-20 12:33:04 +0200

    Write initial User tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_20_073102) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_20_082429) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a first name' do
+    user = described_class.new(first_name: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:first_name]).to include("can't be blank")
+  end
+
+  it 'is invalid without a last name' do
+    user = described_class.new(last_name: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:last_name]).to include("can't be blank")
+  end
+
+  it 'is invalid without an email' do
+    user = described_class.new(email: nil)
+    expect(user).not_to be_valid
+    expect(user.errors[:email]).to include("can't be blank")
+  end
 end
